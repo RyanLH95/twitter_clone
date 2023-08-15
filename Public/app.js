04a250a4d38fb0b978765a33a0fa111c736e65a6
Ryan Henry <126239094+RyanLH95@users.noreply.github.com>
2023-08-15 15:13:40 +0100

    Create an arrow function for posting tweets. Name it const postTweet

--- Public/app.js
+++ Public/app.js
@@ -5,3 +5,20 @@ form.addEventListener('submit', (e) => {
     const tweet = formData.get('tweet-text');
     console.log(tweet);
 })
+
+const postTweet = (tweet) => {
+    const ul = document.querySelector('.tweets-list');
+    const today = new Date();
+    const time = today.toLocaleTimeString();
+    const tweetContent = `
+      <li class="tweet-item">
+        <span class="avatar">A</span>
+        <div class="tweet-details">
+          <strong class="author">Anonymous</strong>
+          <p class="tweet-text">${tweet}</p>
+          <small class="timestamp">${time}</small>
+        </div>
+      </li>
+    `;
+    ul.innerHTML += tweetContent;
+}
